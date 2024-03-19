37f71986dd9db9707affb41662c047dd281d3862
leandroMDS <leandro.mds.21@gmail.com>
2024-03-19 05:57:32 -0300

    test pre-commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home teste</h1>
       <Link href="/about">About</Link>
     </div>
   );
