aa350d2144051c843df8ac1f253bb560c2d255fd
leandroMDS <leandro.mds.21@gmail.com>
2024-03-19 06:27:38 -0300

    adjust husky

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -1,8 +1,10 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Page from '../src/app/(main)/page';
+import Page from '../src/app/(home)/page';
 
 test('Page', () => {
   render(<Page />);
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined();
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Home teste' })
+  ).toBeDefined();
 });
